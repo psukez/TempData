5171c48ab284049ded744c6ccd6c7e122ff5e5e5
psukez <psukez@gmail.com>
2021-01-15 21:27:34 -0300

    update repository

--- tempUpdate.py
+++ tempUpdate.py
@@ -1,8 +1,5 @@
 import subprocess as cmd
 
 cp = cmd.call("git add .", shell=True)
-#print(cp)
-message = "update the repository"
-
-cp = cmd.call("git commit -m '{message}'", shell=True)
+cp = cmd.call("git commit -m 'update repository'", shell=True)
 cp = cmd.call("git push -u origin -f", shell=True)
